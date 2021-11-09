dbfb017b085c182024c8486913630f571737452a
MaximoGismondi <alumno21.gismondi.maximo@ipm.edu.ar>
2021-11-09 09:46:55 -0300

    eu confio mano

--- jenkins/scripts/production.sh
+++ jenkins/scripts/production.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
 npm run build
-firebase deploy -P production --token "$FIREBASE_DEPLOY_TOKEN"
+
+npm install -g firebase-tools
+
+firebase deploy --only hosting --token "$FIREBASE_DEPLOY_TOKEN"
