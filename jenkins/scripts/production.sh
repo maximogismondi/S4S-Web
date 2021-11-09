6c26c0c017d0e0411c217ce36539eebf6c59224b
MaximoGismondi <alumno21.gismondi.maximo@ipm.edu.ar>
2021-11-09 10:21:39 -0300

    ignore engines

--- jenkins/scripts/production.sh
+++ jenkins/scripts/production.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-npm run build
+npm run build 
 
-npm install -g firebase-tools
+npm install -g firebase-tools --ignore-engines
 
 firebase deploy --only hosting --token "$FIREBASE_DEPLOY_TOKEN"
